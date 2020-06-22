07affcdd7de3b5f3a38e679df0899ece6736a678
Arvinder Dhanoa <bender@localhost.localdomain>
2020-06-21 23:57:51 -0500

    update PATH to include .cargo/bin

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,6 @@ if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
 end
+
+#cargo bin
+set -Ua fish_user_paths ~/.cargo/bin
