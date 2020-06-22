2c19510800f54f670eb9219659fe6303c47dea81
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2020-06-22 14:01:13 -0500

    add abbr's and zoxide support in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,19 @@
 # Base16 Shell
 if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
+set BASE16_SHELL "$HOME/.config/base16-shell/"
+source "$BASE16_SHELL/profile_helper.fish"
 end
 
-#cargo bin
+
+
+#Cargo bin
 set -Ua fish_user_paths ~/.cargo/bin
+
+#Zoxide
+source ./zoxide.fish
+
+#Custom abbr's
+if status --is-interactive
+abbr -a ls "exa -a"
+abbr -a lsl "exa -al"
+end
