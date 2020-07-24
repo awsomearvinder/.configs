992e38a7c663173e0d9e3233d97580da11699726
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2020-07-24 17:56:54 -0500

    some env vars

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 # Base16 Shell
 if status --is-interactive
-set BASE16_SHELL "$HOME/.config/base16-shell/"
-source "$BASE16_SHELL/profile_helper.fish"
+	set BASE16_SHELL "$HOME/.config/base16-shell/"
+	source "$BASE16_SHELL/profile_helper.fish"
 end
 
 
@@ -9,12 +9,17 @@ end
 #Cargo bin
 set -Ua fish_user_paths ~/.cargo/bin
 
+#go bin
+set -Ua fish_user_paths ~/go/bin
+
+set -U QT_QPA_PLATFORMTHEME gtk2
+
 #Zoxide
 source ~/.config/fish/zoxide.fish
 
 #Custom abbr's
 if status --is-interactive
-abbr -a ls "exa -a"
-abbr -a lsl "exa -al"
-abbr -a cargod "cargo makedocs --open"
+	abbr -a ls "exa -a"
+	abbr -a lsl "exa -al"
+	abbr -a cargod "cargo makedocs --open"
 end
